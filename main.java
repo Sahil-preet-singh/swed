6f7497c1db80e39ffbaf692a090bcaab05245f7a
Sahil-preet-singh <sahilpreet539@icloud.com>
2026-06-10 19:36:33 +0200

    Update main.java

--- main.java
+++ main.java
@@ -1,32 +1,23 @@
-public class WebsiteMonitor {
-
+package ex4;
+public class Main {
     public static void main(String[] args) {
 
         UserController controller = new UserController();
 
-        
-        controller.registerUser("Sahilpreet_singh", "sahilpreet539@gmail.com");
+        controller.registerUser("Sahilpreet", "Sahilpreet539@gmail.com");
 
-      
-        controller.manageSubscription("subscribe", 1, null,
-            "hhttps://www.frankfurt-university.de", Commchannel.EMAIL, 5);
 
-    
+        controller.manageSubscription("subscribe", 1, null, "https://fuas.de", Commchannel.EMAIL, 5);
+        controller.manageSubscription("cancel", 1, null, "https://fuas.de", Commchannel.EMAIL, 5);
 
-        /
-        System.out.println("\n--- Check cycle ---");
+
+        System.out.println("\n Check cycle ");
         controller.runChecks();
 
-        
-        System.out.println("\n--- Manual notification ---");
+        System.out.println("\n notification: ");
         controller.createNotification(1, "REG-1");
 
-       
-        controller.manageSubscription("update", 1, "REG-1",
-            null, Commchannel.PUSH, 15);
-
-      
-        controller.manageSubscription("cancel", 1, "REG-1",
-            null, null, 0);
+        controller.manageSubscription("update", 1, "REG-1", null, Commchannel.PUSH, 15);
+        controller.manageSubscription("cancel", 1, "REG-1", null, null, 0);
     }
 }
